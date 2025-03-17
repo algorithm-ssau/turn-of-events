11bcf21c9a9eaf6a45b62113c90edce6e2641302
Ilia Lysov <illigiorno@gmail.com>
2025-03-17 20:54:43 +0400

    Add polling

--- parser-service/main.go
+++ parser-service/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/segmentio/kafka-go"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -15,13 +17,13 @@ func main() {
 	// Читаем адрес брокера Kafka из переменной окружения
 	kafkaBroker := os.Getenv("KAFKA_BROKER")
 	if kafkaBroker == "" {
-		kafkaBroker = "kafka:29092" // Адрес по умолчанию для Docker-сети
+		kafkaBroker = "localhost:29092" // Адрес по умолчанию для Docker-сети
 	}
 
 	// Подключение к MongoDB
 	mongoURI := os.Getenv("MONGO_URI")
 	if mongoURI == "" {
-		mongoURI = "mongodb://admin:admin123@mongodb:27017"
+		mongoURI = "mongodb://admin:admin123@localhost:27017"
 	}
 	clientOptions := options.Client().ApplyURI(mongoURI)
 	client, err := mongo.Connect(context.Background(), clientOptions)
@@ -42,25 +44,34 @@ func main() {
 
 	fmt.Println("Ожидание сообщений из Kafka...")
 
-	for {
-		msg, err := reader.ReadMessage(context.Background())
-		if err != nil {
-			log.Printf("Ошибка чтения из Kafka: %v\n", err)
-			continue
-		}
+	// Канал для graceful shutdown
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
 
-		log.Printf("Получено сообщение: %s\n", string(msg.Value))
-
-		// Запуск парсинга при получении команды "start"
-		if string(msg.Value) == "start" {
-			events, err := getEvents()
+	go func() {
+		for {
+			msg, err := reader.ReadMessage(context.Background())
 			if err != nil {
-				log.Printf("Ошибка парсинга: %v\n", err)
+				log.Printf("Ошибка чтения из Kafka: %v\n", err)
 				continue
 			}
 
-			saveEventsToDB(collection, events)
-			log.Println("Парсинг завершён, данные сохранены в БД.")
+			log.Printf("Получено сообщение: %s\n", string(msg.Value))
+
+			// Запуск парсинга по запросу
+			if string(msg.Value) == "start" {
+				events, err := getEvents()
+				if err != nil {
+					log.Printf("Ошибка парсинга: %v\n", err)
+					continue
+				}
+
+				saveEventsToDB(collection, events)
+				log.Println("Парсинг завершён, данные сохранены в БД.")
+			}
 		}
-	}
+	}()
+
+	<-signalChan
+	fmt.Println("Выход по сигналу завершения")
 }
