ff198911be243372c675da2e7af8631918d273ce
Ilia Lysov <illigiorno@gmail.com>
2025-03-30 01:07:36 +0400

    Migratre to fiber

--- parser-service/structures.go
+++ parser-service/structures.go
@@ -1,10 +1,13 @@
 package main
 
+import "time"
+
 type Event struct {
-	Title    string
-	Date     string
-	Time     string
-	Price    string
-	Place    string
-	ImageUrl string
+	Title     string    `json:"title"`
+	Date      string    `json:"date"`
+	Time      string    `json:"time"`
+	Price     string    `json:"price"`
+	Place     string    `json:"place"`
+	ImageUrl  string    `json:"imageUrl"`
+	CreatedAt time.Time `json:"createdAt"`
 }
