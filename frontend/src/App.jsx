1cc88de117c92ea79341d199048065bcf63f5753
Repkol <danildagaev003@gmail.com>
2025-03-31 19:01:56 +0400

    User page и редакт

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,21 @@ import './App.css'
 import Header from './components/Header/Header'
 import Events from './components/Events/Events'
 
+import {User} from './User';
+
 function App() {
   return (
     <>
+    <Router>
       <Header />
       <div className="event-container">
+        <switch>
+          <Route path="/user" component={User} />
+        </switch>
         <Events title="Ближайшие мероприятия" count={3} />
         <Events title="Все мероприятия" count={6} />
       </div>
+    </Router>
     </>
   )
 }
