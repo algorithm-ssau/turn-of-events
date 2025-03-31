2ace70cd3de78ac1beb0c6f4647efdc2b9e0a5dc
Ilia Lysov <illigiorno@gmail.com>
2025-04-01 00:17:43 +0400

    Spring app

--- review-feedback-service/src/main/java/ru/turn_of_events/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.turn_of_events;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ review-feedback-service/src/test/java/ru/turn_of_events/review_backend/ReviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.turn_of_events.review_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
