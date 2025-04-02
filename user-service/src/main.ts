0a4738bc974dc83f1126d32f5489008c5d4d6290
Ilia Lysov <illigiorno@gmail.com>
2025-04-02 22:17:35 +0400

    Port fix

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   // Настраиваем Swagger UI по эндпоинту /api/docs
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
