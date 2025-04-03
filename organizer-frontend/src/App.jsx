9bcc5df29b39dffdf7b8e17ef47a4f493f14b4af
nikipa <nikipagmal@gmail.com>
2025-04-03 18:16:45 +0400

    капец я тупой

--- organizer-frontend/src/App.jsx
+++ organizer-frontend/src/App.jsx
@@ -4,41 +4,42 @@
 //import './App.css'
 
 //export default function App() {
-  //const [count, setCount] = useState(0)
+//const [count, setCount] = useState(0)
 
-  //return (
-    //<>
-    //  <div>
-    //    <a href="https://vite.dev" target="_blank">
-    //      <img src={viteLogo} className="logo" alt="Vite logo" />
-    //    </a>
-    //    <a href="https://react.dev" target="_blank">
-    //      <img src={reactLogo} className="logo react" alt="React logo" />
-    //    </a>
-    //  </div>
-    //  <h1>Vite + React</h1>
-    //  <div className="card">
-    //    <button onClick={() => setCount((count) => count + 1)}>
-    //      count is {count}
-    //    </button>
-    //    <p>
-    //      Edit <code>src/App.jsx</code> and save to test HMR
-    //    </p>
-    //  </div>
-    //  <p className="read-the-docs">
-    //    Click on the Vite and React logos to learn more
-    //  </p>
-      //</>
-  //)
+//return (
+//<>
+//  <div>
+//    <a href="https://vite.dev" target="_blank">
+//      <img src={viteLogo} className="logo" alt="Vite logo" />
+//    </a>
+//    <a href="https://react.dev" target="_blank">
+//      <img src={reactLogo} className="logo react" alt="React logo" />
+//    </a>
+//  </div>
+//  <h1>Vite + React</h1>
+//  <div className="card">
+//    <button onClick={() => setCount((count) => count + 1)}>
+//      count is {count}
+//    </button>
+//    <p>
+//      Edit <code>src/App.jsx</code> and save to test HMR
+//    </p>
+//  </div>
+//  <p className="read-the-docs">
+//    Click on the Vite and React logos to learn more
+//  </p>
+//</>
+//)
 //}
 
 
 
 import './App.css';
 import React from "react";
-import img from "./img.jpg"
-import img2 from "./img2.jpg"
-import img3 from "./img3.jpg"
+import imgcr from "./create.jpg";
+import img from "./img.jpg";
+import img2 from "./img2.jpg";
+import img3 from "./img3.jpg";
 
 const EventBox = ({ imgg, text }) => {
     return (
@@ -48,30 +49,30 @@ const EventBox = ({ imgg, text }) => {
             background: "#555555",
             border: "2px solid gray",
             borderRadius: "40px",
-            margin:"40px"
-} }>
+            margin: "40px"
+        }}>
 
-            <img src={imgg} style={{
+            <img alt="it sucks((" src={imgg} style={{
                 width: "300px",
                 height: "300px",
                 borderRadius: "38px 38px 0px 0px"
-} } />
+            }} />
             <div style={{
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center"
-} }> {text}</div>
+            }}> {text}</div>
         </div>
     );
 };
 export default function App() {
-    
+
     return (
         <div style={{ display: "flex" }}>
-            <EventBox text={img, "создать мероприятие"} />
-            <EventBox text={img, "одно крутое мероприятие"}  />
-            <EventBox text={img2, "второе крутое мероприятие"} />
-            <EventBox text={img3, "еще одно мероприятие"} />
+            <EventBox imgg={imgcr} text="создать мероприятие" />
+            <EventBox imgg={img} text="одно крутое мероприятие"  />
+            <EventBox imgg={img2} text="второе крутое мероприятие" />
+            <EventBox imgg={img3} text="еще одно мероприятие" />
         </div>
     )
 }
