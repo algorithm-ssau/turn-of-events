e65437fb7b1ca6fb62eede59fabe834dc4c080fa
Ilia Lysov <illigiorno@gmail.com>
2025-04-12 18:06:44 +0400

    Gateway v1

---
+++ api-gateway/src/main/java/com/turnofevents/apigateway/config/RouteConfig.java
@@ -0,0 +1,29 @@
+package com.turnofevents.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // Добавляем дополнительный маршрут для публичных апи мероприятий (доступные без авторизации)
+                .route("public-events", r -> r
+                        .path("/api/public/events/**")
+                        .and()
+                        .method(HttpMethod.GET)
+                        .uri("http://event-service:8080")
+                )
+                // Добавляем маршрут для swagger-документации
+                .route("api-docs", r -> r
+                        .path("/api-docs/**")
+                        .uri("forward:/api-docs")
+                )
+                .build();
+    }
+} 
