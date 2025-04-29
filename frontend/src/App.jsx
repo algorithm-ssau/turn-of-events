f79b279b364657c5ab8189e0b0af65862a36b903
F4NT0M <evgeniylebedev55@gmail.com>
2025-04-30 02:45:59 +0400

    Пропись Route к странице с подробностями о мероприятии

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header'
 import Events from './components/Events/Events'
 import UpcomingEvents from './components/UpcomingEvents/UpcomingEvents'
+import EventDetails from './components/EventDetails/EventDetails'
 import {User} from './User';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <div className="event-container">
-        <UpcomingEvents title="Ближайшие мероприятия" count={10} />
-        <Events title="Все мероприятия" count={9} />
-      </div>
-    </>
+      <Routes>
+        <Route path="/" element={
+          <div className="event-container">
+            <UpcomingEvents title="Ближайшие мероприятия" count={10} />
+            <Events title="Все мероприятия" count={9} />
+          </div>
+        } />
+        <Route path="/event/:id" element={<EventDetails />} />
+      </Routes>
+    </Router>
   )
 }
 
