ba4fc467df080396a8a17ef940ecc7ff4d959b4d
F4NT0M <evgeniylebedev55@gmail.com>
2025-05-16 01:07:44 +0400

    Изменение выпадающего меню

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header'
 import Events from './components/Events/Events'
